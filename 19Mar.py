962e6c0369efb9b73aab616b96b863ccdb15d26e
NNadiia <nadiianazarchuk@gmail.com>
2026-03-23 13:40:34 +0100

    deleted one part

--- 19Mar.py
+++ 19Mar.py
@@ -12,3 +12,22 @@ while True:
     num_dict = {}
     for i in range(1, 4):
      num_dict[i] = str(i)
+
+
+some_string = "apple banana apple orange banana apple"
+split = some_string.split()
+counter = {}
+for word in split:
+    if word not in counter:
+        counter[word] = 1
+    else:
+        counter[word] += 1
+print(counter)
+
+
+
+a = [1, 2, 3, 4]
+b = [3, 4, 5, 6]
+common = list(set(a) & set(b))
+print(common)
+
